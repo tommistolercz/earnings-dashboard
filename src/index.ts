964a6a3552ba6d1817e063fb5e4cc924e5213892
Tomas Mistoler <tomas.mistoler@gmail.com>
2025-07-12 20:45:32 +0200

    chore: google auth fix in prod

--- src/index.ts
+++ src/index.ts
@@ -35,6 +35,7 @@ app.use(session({
     cookie: {
         secure: process.env.NODE_ENV === "production", // https in production only
         httpOnly: true,
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
         maxAge: 1000 * 60 * 60 * 24 * 7 // 7d
     }
 }));
