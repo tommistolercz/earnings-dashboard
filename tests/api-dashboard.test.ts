eba9c01ed1c4a3169d1bf329eb926dbeb23001d3
Tomas Mistoler <tomas.mistoler@gmail.com>
2025-07-16 19:30:57 +0200

    chore: migrated to luxon from date-fns

--- tests/api-dashboard.test.ts
+++ tests/api-dashboard.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { UserSettings } from "../src/routes/api/api-settings";
-import { TZDate } from "@date-fns/tz";
+import { DateTime } from "luxon";
 
 import * as apiDashboard from "../src/routes/api/api-dashboard";
 
@@ -20,20 +20,22 @@ describe("getIsWeekend()", () => {
 
     // monday
     it("returns false for monday", () => {
-        const monday = new TZDate(new Date("2025-07-07"), testSettings.timeZone);
+        const monday = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 7
+        }, { zone: testSettings.timeZone });
         expect(apiDashboard.getIsWeekend(monday)).toBe(false);
     });
 
     // saturday
     it("returns true for saturday", () => {
-        const saturday = new TZDate(new Date("2025-07-12"), testSettings.timeZone);
+        const saturday = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 12
+        }, { zone: testSettings.timeZone });
         expect(apiDashboard.getIsWeekend(saturday)).toBe(true);
-    });
-
-    // sunday
-    it("returns true for sunday", () => {
-        const sunday = new TZDate(new Date("2025-07-13"), testSettings.timeZone);
-        expect(apiDashboard.getIsWeekend(sunday)).toBe(true);
     });
 });
 
@@ -42,19 +44,31 @@ describe("getIsHoliday()", () => {
 
     // non-holiday
     it("returns false for a non-holiday", () => {
-        const nonHoliday = new TZDate(new Date("2025-07-07"), testSettings.timeZone);  // mon 2025-07-07
+        const nonHoliday = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 7
+        }, { zone: testSettings.timeZone });  // mon 2025-07-07
         expect(apiDashboard.getIsHoliday(nonHoliday, testSettings)).toBe(false);
     });
 
     // non-public holiday
     it("returns false for a non-public holiday", () => {
-        const nonPublicHoliday = new TZDate(new Date("2025-04-17"), testSettings.timeZone);  // easter maudy thursday
+        const nonPublicHoliday = DateTime.fromObject({
+            year: 2025,
+            month: 4,
+            day: 17
+        }, { zone: testSettings.timeZone });  // easter maudy thursday
         expect(apiDashboard.getIsHoliday(nonPublicHoliday, testSettings)).toBe(false);
     });
 
     // public holiday
     it("returns true for a public holiday", () => {
-        const publicHoliday = new TZDate(new Date("2025-04-18"), testSettings.timeZone);  // easter good friday
+        const publicHoliday = DateTime.fromObject({
+            year: 2025,
+            month: 4,
+            day: 18
+        }, { zone: testSettings.timeZone });  // easter good friday
         expect(apiDashboard.getIsHoliday(publicHoliday, testSettings)).toBe(true);
     });
 });
@@ -64,25 +78,43 @@ describe("getIsEarningTime()", () => {
 
     // work hour
     it("returns true for a work hour", () => {
-        const workHour = new TZDate(new Date(2025, 6, 7, testSettings.workHoursStart, 0, 0), testSettings.timeZone);  // mon 2025-07-07
+        const workHour = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 7,
+            hour: testSettings.workHoursStart
+        }, { zone: testSettings.timeZone });  // mon 2025-07-07 9:00
         expect(apiDashboard.getIsEarningTime(workHour, testSettings)).toBe(true);
     });
 
     // outside work hours
     it("returns false for outside work hours", () => {
-        const outsideWorkHours = new TZDate(new Date(2025, 6, 7, testSettings.workHoursEnd, 0, 0), testSettings.timeZone);  // mon 2025-07-07
+        const outsideWorkHours = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 7,
+            hour: testSettings.workHoursEnd + 1
+        }, { zone: testSettings.timeZone });  // mon 2025-07-07 18:00
         expect(apiDashboard.getIsEarningTime(outsideWorkHours, testSettings)).toBe(false);
     });
 
     // weekend
     it("returns false for a weekend", () => {
-        const weekend = new TZDate(new Date("2025-07-12T12:00:00"), testSettings.timeZone);  // sat 2025-07-12
+        const weekend = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 12
+        }, { zone: testSettings.timeZone });  // sat 2025-07-12
         expect(apiDashboard.getIsEarningTime(weekend, testSettings)).toBe(false);
     });
 
     // holiday
     it("returns false for a holiday", () => {
-        const holiday = new TZDate(new Date("2025-04-18T12:00:00"), testSettings.timeZone);  // easter good friday
+        const holiday = DateTime.fromObject({
+            year: 2025,
+            month: 4,
+            day: 18
+        }, { zone: testSettings.timeZone });  // easter good friday
         expect(apiDashboard.getIsEarningTime(holiday, testSettings)).toBe(false);
     });
 });
@@ -92,13 +124,21 @@ describe("getWorkingDaysInMonth()", () => {
 
     // month with 20 working days
     it("returns 20 for a month with 20 working days", () => {
-        const date = new TZDate(new Date("2025-04-15"), testSettings.timeZone);
+        const date = DateTime.fromObject({
+            year: 2025,
+            month: 4,
+            day: 1
+        }, { zone: testSettings.timeZone });  // apr 2025
         expect(apiDashboard.getWorkingDaysInMonth(date, testSettings)).toBe(20);
     });
 
     // month with 23 working days
     it("returns 23 for a month with 23 working days", () => {
-        const date = new TZDate(new Date("2025-07-15"), testSettings.timeZone);
+        const date = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 1
+        }, { zone: testSettings.timeZone });  // jul 2025
         expect(apiDashboard.getWorkingDaysInMonth(date, testSettings)).toBe(23);
     });
 });
@@ -108,20 +148,37 @@ describe("getCurrentEarnings()", () => {
 
     // zero earnings 1 minute before working hours in a first working day of the month
     it("returns zero earnings 1 minute before working hours in a first working day of the month", () => {
-        const date = new TZDate(new Date(2025, 6, 1, testSettings.workHoursStart - 1, 59, 0), testSettings.timeZone); // tue 2025-07-01
+        const date = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 1,
+            hour: testSettings.workHoursStart - 1,
+            minute: 59
+        }, { zone: testSettings.timeZone });  // tue 2025-07-01 8:59
         expect(apiDashboard.getCurrentEarnings(date, testSettings)).toBe(0);
     });
 
     // earnings for 1 hour of work in a first working day of the month
     it("returns earnings for 1 hour of work in a first working day of the month", () => {
-        const date = new TZDate(new Date(2025, 6, 1, testSettings.workHoursStart + 1, 0, 0), testSettings.timeZone);  // tue 2025-07-01
-        const expectedEarnings = Math.floor(testSettings.mandayRate / 8); // 1 hour of work
+        const date = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 1,
+            hour: testSettings.workHoursStart + 1,
+            minute: 0
+        }, { zone: testSettings.timeZone });  // tue 2025-07-01 10:00
+        const expectedEarnings = Math.floor(testSettings.mandayRate / 8);
         expect(apiDashboard.getCurrentEarnings(date, testSettings)).toBe(expectedEarnings);
     });
 
     // earnings for two and a half days of work
     it("returns earnings for two and a half days of work", () => {
-        const date = new TZDate(new Date(2025, 6, 3, testSettings.workHoursStart + 4, 0, 0), testSettings.timeZone);  // thu 2025-07-03
+        const date = DateTime.fromObject({
+            year: 2025,
+            month: 7,
+            day: 3,
+            hour: testSettings.workHoursStart + 4,
+        }, { zone: testSettings.timeZone });  // thu 2025-07-03 13:00
         const expectedEarnings = Math.floor(testSettings.mandayRate * 2.5);
         expect(apiDashboard.getCurrentEarnings(date, testSettings)).toBe(expectedEarnings);
     });
